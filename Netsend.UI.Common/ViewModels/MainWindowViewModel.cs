3208ed60471746277a722c6612910db07845007d
Caleb Stein <caleb@calebstein.net>
2024-01-27 14:36:34 -0800

    added comments

--- Netsend.UI.Common/ViewModels/MainWindowViewModel.cs
+++ Netsend.UI.Common/ViewModels/MainWindowViewModel.cs
@@ -22,14 +22,19 @@ public class MainWindowViewModel : ViewModelBase
 
     private void ClientsUpdated(object? sender, NotifyCollectionChangedEventArgs e)
     {
+        // The idea here is to track our own list to display, rather than pull the list from BackgroundServices.Worker directly.
+        // This is good because we don't need things like the pingCounter in the UI, and the backend logic doesn't care about
+        // UI-specific things like the OS icon file. It also smooths over threading concerns between the Worker and UI threads.
         if (e.NewItems != null)
         {
             foreach (ClientInfo c in e.NewItems)
             {
+                // I find switch expressions to be more clear than big if/else if/else blocks; this is just grabbing the proper
+                // icon file based in the information in the OS string.
                 var iconFile = c.Client.OS switch
                 {
                     { } a when a.Contains("Windows") => "windows.png",
-                    { } a when a.Contains("OSX") => "apple.png",
+                    { } a when a.Contains("Mac") => "apple.png",
                     _ => "tencent-qq.png"
                 };
                 var iconPath = ImageHelper.LoadFromResource(new Uri($"{BaseIconPath}/{iconFile}"));
